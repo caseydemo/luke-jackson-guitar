7308080308548bc93ed6ecb08c1106f44ce58947
Casey DeMattina <caseydemattina@Caseys-MacBook-Air-2.local>
2024-12-01 10:39:28 -0500

    create header and add to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 import Hero from "./components/Hero";
+import Header from "./components/Header";
 
 export default function Home() {
     return (
         <div className={styles.page}>
+            <Header />
             <Hero />
         </div>
     )
