9eff9771a0a8dc4409f5f499aa9152535e24925c
Casey DeMattina <caseydemattina@Caseys-MacBook-Air-2.local>
2024-12-04 20:13:45 -0500

    added lorem ipsum to services data

--- app/data/servicesData.ts
+++ app/data/servicesData.ts
@@ -1,9 +1,17 @@
 import { ServiceItemProps } from "../types";
+/*
+  Services
+  - private lessons
+  - performance
+  - composition
+  - about
+*/
 
+const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 export const servicesData: ServiceItemProps[] = [
   {
-    title: "about",
-    description: "this is where we will put a brief intro to the client, and talk a little about luke",
+    title: "Private Instruction",
+    description: lorem,
     featured_image: "/luke_hero_image_1.jpg",
     images: [
       "/quartet_superheros.jpg",
@@ -12,8 +20,8 @@ export const servicesData: ServiceItemProps[] = [
     ]
   },
   {
-    title: "about",
-    description: "this is where we will put a brief intro to the client, and talk a little about luke",
+    title: "Performance",
+    description: lorem,
     featured_image: "/luke_hero_image_1.jpg",
     images: [
       "/quartet_superheros.jpg",
@@ -22,8 +30,8 @@ export const servicesData: ServiceItemProps[] = [
     ]
   },
   {
-    title: "about",
-    description: "this is where we will put a brief intro to the client, and talk a little about luke",
+    title: "Composition",
+    description: lorem,
     featured_image: "/luke_hero_image_1.jpg",
     images: [
       "/quartet_superheros.jpg",
@@ -32,8 +40,8 @@ export const servicesData: ServiceItemProps[] = [
     ]
   },
   {
-    title: "about",
-    description: "this is where we will put a brief intro to the client, and talk a little about luke",
+    title: "About Luke",
+    description: lorem,
     featured_image: "/luke_hero_image_1.jpg",
     images: [
       "/quartet_superheros.jpg",
